c2320197dc6db76bf0e4d2ab5c73ad011630bc85
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-12 13:51:33 +0300

    Userinterface oluşturuldu.

---
+++ ArtBook/Controller/SecondVC.swift
@@ -0,0 +1,29 @@
+//
+//  SecondVC.swift
+//  ArtBook
+//
+//  Created by Büşra Özkan on 12.11.2022.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var artistTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+}
